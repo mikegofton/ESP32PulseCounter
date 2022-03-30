00f40a6ed9b440cf85ff3edb24c74f698b04071c
mike gofton <mike.gofton@gmail.com>
2022-03-30 22:19:09 +1000

    Initial commit

---
+++ include/esp32_pcnt.h
@@ -0,0 +1,55 @@
+/*
+esp32_pcnt.h is a wrapper for the hardware pulse counter devices in the Espressif ESP32 SoC
+
+The PCNT (Pulse Counter) module is designed to count the number of rising and/or falling edges of an input signal.
+Each pulse counter unit has a 16-bit signed counter register and two channels which can be configured
+to either increment or decrement the counter.
+Each channel has a signal input that accepts signal edges to be detected, as well as a control input which can be used 
+to enable or disable the signal input.
+The count and control inputs have optional filters that can be used to discard unwanted glitches in the signal.
+There are five counter state watch events which are able to trigger an interrupt. The event happens on the pulse counter
+reaching specific values: minimum or maximum count values, two threshold values and counter = zero.
+
+This library is coded for the Arduino Framework and is based on ESP-IDF relese 4.4
+Additional ESP=IDF documentation is available here 
+https://docs.espressif.com/projects/esp-idf/en/release-v4.4/esp32/api-reference/peripherals/pcnt.html
+
+This software is released to the public domain under the MIT Licence, copyright (c) 2022 Mike Gofton.
+*/
+
+#ifndef PulseCounter_h
+#define PulseCounter_h
+
+#include <Arduino.h>
+#include <driver/pcnt.h>
+
+class PulseCounter
+{
+    public:
+        void initialise(uint8_t PCNT_UNIT, uint8_t PCNT_CHANNEL, int PCNT_INPUT_SIG_IO, int PCNT_INPUT_CTRL_IO);
+        void set_mode(pcnt_count_mode_t pos_mode, pcnt_count_mode_t neg_mode, pcnt_ctrl_mode_t hctrl_mode, pcnt_ctrl_mode_t lctrl_mode);
+        int16_t get_value();
+        void pause();
+        void resume();
+        void clear();
+        void interrupt_enable();
+        void interrupt_disable();
+        void event_enable(pcnt_evt_type_t evt_type);
+        void event_disable(pcnt_evt_type_t evt_type);
+        void set_event_value(pcnt_evt_type_t evt_type, int16_t value);
+        void get_event_value(pcnt_evt_type_t evt_type, int16_t *value);
+        void isr_register(void (*fn)(void *), void *arg, int intr_alloc_flags);
+        void isr_unregister();
+        void set_filter_value(uint16_t filter_val);
+        void filter_enable();
+        void filter_disable();
+    private:
+        pcnt_unit_t _unit;
+        pcnt_channel_t _channel;
+        int16_t _sig_pin , _ctrl_pin ;
+        pcnt_count_mode_t _pos_mode , _neg_mode;
+        pcnt_ctrl_mode_t _hctrl_mode , lctrl_mode;
+        pcnt_isr_handle_t _isr_handle;
+        pcnt_config_t pcnt_config;
+};
+#endif
